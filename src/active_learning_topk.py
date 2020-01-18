44a6c365b79b70c8f9c898f7c69bb1b709075896
Robert L. Logan IV <rloganiv@gmail.com>
2020-01-18 13:52:44 -0800

    Fixed lingering merge conflict

--- src/active_learning_topk.py
+++ src/active_learning_topk.py
@@ -149,19 +149,12 @@ def eval(args: argparse.Namespace,
 
     elif metric == 'calibration_error':
         model = ClasswiseEce(num_classes, num_bins=10, weight=weight, prior=None)
-<<<<<<< HEAD
 
     avg_num_agreement = np.zeros((num_samples,))
     cumulative_metric = np.zeros((num_samples,))
     non_cumulative_metric = np.zeros((num_samples,))
 
     topk_arms = np.zeros((num_classes,), dtype=np.bool_)
-=======
-
-    avg_num_agreement = np.zeros((num_samples,))
-    cumulative_metric = np.zeros((num_samples,))
-    non_cumulative_metric = np.zeros((num_samples,))
->>>>>>> origin/master
 
     for idx, (category, observation, confidence) in enumerate(zip(categories, observations, confidences)):
 
